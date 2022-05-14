ec2a9fb8f8e0a09d95f65c2b9b02d5d1c7628db3
Aino Lehtimäki <lehtimki.aino@gmail.com>
2022-05-14 21:34:50 +0300

    Started to use wordpress, pokemon app as a base

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+// Heroku dynamically sets a port
+const PORT = process.env.PORT || 5000
+
+app.use(express.static('dist'))
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/ping', (req, res) => {
+  res.send('pong')
+})
+
+app.get('/version', (req, res) => {
+  res.send('8') // change this string to ensure a new version deployed
+})
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('server started on port 5000')
+})
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <p>hello world</p>
+    </div>
+  )
+}
+
+export default App
